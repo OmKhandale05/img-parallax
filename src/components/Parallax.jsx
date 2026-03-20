2b4217862578821f3c9ae80a6f96a245418c12e2
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-20 17:03:27 +0530

    Create layout for parallax img effect

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,6 +1,34 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useLayoutEffect } from "react";
+
 const Parallax = () => {
+
+    useLayoutEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+    }, []);
     return ( 
-        <div><h1>parallax effect</h1></div>
+        <div className="bg-black text-white">
+            <div className="h-[100vh] flex items-center justify-center">
+                <h1 className="text-4xl">Scroll Down</h1>
+            </div>
+
+            <div className="parallax-section h-[200vh] relative overflow-hidden">
+                <img 
+                src="https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+                className="bg-layer absolute w-full h-full object-cover" />
+
+                <img 
+                src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee" 
+                className="mid-layer absolute w-full -h-full object-cover opacity-70" />
+
+                <h1 className="front-layer text-6xl font-bold absolute top--1/2 left-1/2 -translate-x-1/2">
+                    Parallax
+                </h1>
+
+            </div>
+
+        </div>
      );
 }
  
