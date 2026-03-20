9c2b6af421632ba911891796bf370d244fcd6a6f
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-20 17:21:36 +0530

    Fix parallax text centering using flex box

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -45,7 +45,7 @@ const Parallax = () => {
                 <h1 className="text-4xl">Scroll Down</h1>
             </div>
 
-            <div className="parallax-section h-[200vh] relative overflow-hidden">
+            <div className="parallax-section h-[200vh] bg-black relative overflow-hidden flex items-center justify-center">
                 <img 
                 src="https://images.unsplash.com/photo-1501785888041-af3ef285b470"
                 className="bg-layer absolute w-full h-full object-cover z-0" />
@@ -54,7 +54,7 @@ const Parallax = () => {
                 src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee" 
                 className="mid-layer absolute w-full -h-full object-cover opacity-70 z-10" />
 
-                <h1 className="front-layer text-6xl font-bold absolute top--1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+                <h1 className="front-layer text-6xl font-bold text-center z-20">
                     Parallax
                 </h1>
 
